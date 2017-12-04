a80784a4d8a24e588b0c743cfebf7aec1254b288
Johnston Jiaa <jjiaa@fb.com>
2017-12-04 12:43:01 -0500

    Import the F8 2017 app

---
+++ android/app/build/generated/source/buildConfig/release/com/facebook/f8/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.facebook.f8;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.facebook.f8";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10000410;
+  public static final String VERSION_NAME = "4.0.0";
+}
